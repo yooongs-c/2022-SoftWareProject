a955817b396e18f8de491a6a5dcfbe292b604129
comeherenow <oseyoung@naver.com>
2022-06-02 01:21:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,6 @@ import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch, Routes } from "react-router-dom";
 
 function App() {
-   // useState를 사용하여 open상태를 변경한다. (open일때 true로 만들어 열리는 방식)
-   const [modalOpen, setModalOpen] = useState(false);
-
-   const openModal = () => {
-     setModalOpen(true);
-   };
-   const closeModal = () => {
-     setModalOpen(false);
-   };
 
   return(
       <div>
